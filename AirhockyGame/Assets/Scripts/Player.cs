7366e721546b980b2c976d233445f06a254c702e
Hagens,Ruud R <ruud.hagens@hotmail.com>
2016-04-21 16:11:37 +0200

    air hockey movement

---
+++ AirhockyGame/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private int speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.position += new Vector3(0, -speed, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
+    }
+}
