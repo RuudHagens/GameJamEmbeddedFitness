84c7faceb520378b422844c8a755b6fccee2c5c5
Anton van Dijk <anton.van.dijk@live.nl>
2016-04-21 16:13:16 +0200

    Added camera and player movement

---
+++ ZoekplaatjeGame/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        public Transform Target;
+
+        private void Awake()
+        {
+            if (!Target)
+            {
+                Debug.LogWarning("No target set for FollowCamera to follow.");
+                enabled = false;
+            }
+        }
+
+        // Use this for initialization
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            transform.position = Target.position;
+        }
+    }
+}
---
+++ ZoekplaatjeGame/Assets/Scripts/InputMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class InputMovement : MonoBehaviour
+    {
+        public float MoveSpeed;
+        private Rigidbody2D body;
+
+        private void Awake()
+        {
+            
+        }
+
+        // Use this for initialization
+        private void Start()
+        {
+            body = GetComponent<Rigidbody2D>();
+            if (!body)
+            {
+                Debug.LogWarning(string.Format("Object {0} does not have a rigidbody.", name));
+                enabled = false;
+            }
+        }
+
+        private void Reset()
+        {
+            MoveSpeed = 1;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            body.AddForce(new Vector3(Input.GetAxis("Horizontal") * MoveSpeed, Input.GetAxis("Vertical") * MoveSpeed), ForceMode2D.Impulse);
+        }
+    }
+}
