382a95d88c6885ba8ab7df1553c303851f1b06b3
Schilders,Koen K.N.D <koen.schilders@student.fontys.nl>
2016-04-21 15:41:25 +0200

    Setup managers, added wall sprite

---
+++ TrefbalGame/Assets/Scripts/HudManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudManager : MonoBehaviour
+{
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start () {
+	
+	}
+	
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
+	void Update () {
+	
+	}
+}
