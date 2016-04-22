98dea7b8074038b94e8ac5ca755862de290dd35f
Hagens,Ruud R <ruud.hagens@hotmail.com>
2016-04-22 14:39:42 +0200

    spawn sphere ready to test

---
+++ AirhockyGame/Assets/Scripts/mergedSphere.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class mergedSphere : MonoBehaviour {
+
+    public GameObject sphere;
+    private List<GameObject> collidedPlayers;
+    private GameObject s;
+
+    // Use this for initialization
+    void Start () {
+        collidedPlayers = new List<GameObject>();
+        s = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(collidedPlayers.Count != 0 && s != null)
+        {
+            Vector3 playerAverageLocation = new Vector3(0, 0, 0);
+            foreach (GameObject player in collidedPlayers)
+            {
+                playerAverageLocation += player.transform.position;
+            }
+            playerAverageLocation /= collidedPlayers.Count;
+            s.transform.position = playerAverageLocation;
+        }
+	}
+
+    /// <summary>
+    /// Draws a sphere around the average position of the players
+    /// </summary>
+    /// <param name="players"></param>
+    public void spawnSphere(List<GameObject> players)
+    {
+        collidedPlayers = players;
+        Vector3 playerAverageLocation = new Vector3(0, 0, 0);
+        foreach (GameObject player in players)
+        {
+            playerAverageLocation += player.transform.position;
+        }
+        playerAverageLocation /= players.Count;
+
+        s = (GameObject)Instantiate(sphere, playerAverageLocation, Quaternion.identity);
+        s.transform.localScale *= players.Count;
+    }
+}
