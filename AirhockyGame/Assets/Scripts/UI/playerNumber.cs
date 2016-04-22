4018e64287338e0833978238a13e6a27cc529d5f
Martijn Daniels <martijn.daniels@student.fontys.nl>
2016-04-22 14:49:17 +0200

    basis nummer assignment

---
+++ AirhockyGame/Assets/Scripts/UI/playerNumber.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerNumber : MonoBehaviour {
+
+
+    public GameObject Player;
+    private Player player;
+    private Text t;
+    private RectTransform rect;
+	void Start () {
+        player = Player.GetComponent<Player>();
+        t = gameObject.GetComponent<Text>();
+        rect = t.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        t.text = player.GetNumber().ToString();
+//        transform.position.Set(player.transform.position.x, player.transform.position.y, transform.position.z);
+        rect.position = new Vector3(player.transform.position.x, player.transform.position.y+5, transform.position.z);
+        Debug.Log("Playerpos "+player.transform.position.x+","+ player.transform.position.y);
+        Debug.Log("Pos is now " + rect.position.x + "," + rect.position.y);
+    }
+
+    public void SetPlayer(GameObject p)
+    {
+        Player = p;
+        player = p.GetComponent<Player>();
+    }
+
+    public void SetPlayer(Player p)
+    {
+        player = p;
+        Player = p.gameObject.transform.parent.gameObject;
+    }
+}
--- AirhockyGame/Assets/Scripts/_gameMGR.cs
+++ AirhockyGame/Assets/Scripts/_gameMGR.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using Assets.Scripts.helpers;
+using System;
 
 public class _gameMGR : MonoBehaviour {
 
 
     private int scoreR;
     private int scoreB;
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+    public GameObject p4;
+    private List<Player> players;
+    public int MinSum;
+    public int MaxSum;
+    private int currAnswer;
 
     void Start()
     {
-        
+        players = new List<Player>();
+        //hardcoded voor demo
+        players.Add(p1.GetComponent<Player>());
+        players.Add(p2.GetComponent<Player>());
+        players.Add(p3.GetComponent<Player>());
+        players.Add(p4.GetComponent<Player>());
+        GenerateNewAnswer();
     }
 
     public void AddScore(Team t)
@@ -24,8 +40,10 @@ public class _gameMGR : MonoBehaviour {
                 scoreB++;
                 break;
             default:
+                Debug.LogError("Score added for invalid team " + t);
                 break;
         }
+        GenerateNewAnswer();
     }
 
     public int GetScore(Team t)
@@ -39,8 +57,34 @@ public class _gameMGR : MonoBehaviour {
                 return scoreB;
                 break;
             default:
-                throw new UnityException();
+                return 0;
+                Debug.LogError("Score queried for invalid team " + t);
                 break;
         }
+    }
+
+    public void GenerateNewAnswer()
+    {
+        System.Random rA = new System.Random();
+        System.Random r1 = new System.Random();
+        currAnswer = rA.Next(MinSum, MaxSum);
+        int eh = 0;//the # that will be given to the player
+        RandomHelper.ShuffleList<Player>(players);
+        int c = 0;
+        foreach (Player p in players)
+        {
+            if(c%2==0|c==0)
+            {
+                eh = r1.Next(1, currAnswer);
+            }
+            else { eh = currAnswer - eh; }
+            p.SetNumber(eh);
+            c++;
+        }
+    }
+
+    public int GetAnswer()
+    {
+        return currAnswer;
     }
 }
