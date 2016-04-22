88cd921f2a8317d3084dd8961de8ba38b3a63815
KeesWerson <kees-werson@hotmail.com>
2016-04-22 10:40:48 +0200

    Players can move

--- TrefbalGame/Assets/BallManager.cs
+++ TrefbalGame/Assets/BallManager.cs
@@ -12,48 +12,77 @@ public class BallManager : MonoBehaviour {
     public float speedMultiplier;
     private Vector3 velocity = Vector3.zero;
 
-    private bool moving = false;
+    private GameObject currentSelectedPlayer;
+    public static BallManager instance;
+
+    private bool movingBall = false;
+    private bool movingPeople = true;
 
     // Use this for initialization
     void Start () {
         if(speedMultiplier == 0)
         {
             speedMultiplier = 1;
-        } 
+        }
+
+        instance = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0) && !moving)
+        if (Input.GetMouseButton(0))
         {
-            Debug.Log("Pressed left click.");
-
             mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
             p = camera.ScreenToWorldPoint(mousePosition);
             p.z = 0;
-
-            direction = new Vector2(p.x - transform.position.x, p.y - transform.position.y);
-
-            /*Debug.Log("Pos Ball: " + transform.position.x + ", " + transform.position.y);
-            Debug.Log("Click point: " + p);
-            Debug.Log("Final Direction: " + direction);*/
-
-            direction.Normalize();
-            moving = true;
         }
 
+        if (Input.GetMouseButtonDown(0) && !movingBall)
+        {
+            Debug.Log("Pressed left click.");
+            if (movingPeople)
+            {
+
+            }
+            else
+            {
+                direction = new Vector2(p.x - transform.position.x, p.y - transform.position.y);
+
+                /*Debug.Log("Pos Ball: " + transform.position.x + ", " + transform.position.y);
+                Debug.Log("Click point: " + p);
+                Debug.Log("Final Direction: " + direction);*/
+
+                direction.Normalize();
+                movingBall = true;
+            }          
+        }
+
+        movePlayer();
         moveBall();
+    }
+
+    /// <summary>
+    /// When clicked on player, that player will be the currentSelectedPlayer
+    /// </summary>
+    public void selectThisPlayer(GameObject obj)
+    {    
+        currentSelectedPlayer = obj;
     }
 
     void moveBall()
     {
-        if (moving)
+        if (movingBall)
         {
-            /*this.transform.position = p;
-            this.transform.position = p;*/
-
-            //this.transform.position = Vector3.SmoothDamp(transform.position, p, ref velocity, smoothTime);
             transform.Translate(direction * Time.deltaTime * speedMultiplier, Space.World);
+        }
+    }
+
+    void movePlayer()
+    {
+        if (!movingBall && currentSelectedPlayer != null)
+        {
+            Debug.Log("Ik beweeg de speler");
+            currentSelectedPlayer.transform.position = p;
         }
     }
 
@@ -67,6 +96,6 @@ public class BallManager : MonoBehaviour {
         {
             Debug.Log("Bal gevangen");    
         }
-        moving = false;
+        movingBall = false;
     }
 }
