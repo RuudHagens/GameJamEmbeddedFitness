ea31e76149867937f5b8d92e923e14ba41eb413c
Hagens,Ruud R <ruud.hagens@hotmail.com>
2016-04-22 10:43:15 +0200

    air hockey score added

---
+++ AirhockyGame/Assets/Scripts/_gameMGR.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class _gameMGR : MonoBehaviour {
+
+
+    private int scoreR;
+    private int scoreB;
+
+    void Start()
+    {
+        
+    }
+
+    public void AddScore(Team t)
+    {
+        Debug.Log("Score added for " + t);
+        switch (t)//yay hacky code :DDDDD
+        {
+            case Team.red:
+                scoreR++;
+                break;
+            case Team.blue:
+                scoreB++;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public int GetScore(Team t)
+    {
+        switch (t)//yay hacky code :DDDDD
+        {
+            case Team.red:
+                return scoreR;
+                break;
+            case Team.blue:
+                return scoreB;
+                break;
+            default:
+                throw new UnityException();
+                break;
+        }
+    }
+}
---
+++ AirhockyGame/Assets/Scripts/goal.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum Team
+{
+    red,
+    blue
+}
+
+public class goal : MonoBehaviour {
+    public Team team;
+    public GameObject gameMGR;
+    private _gameMGR mgr;
+
+    void Start()
+    {
+        mgr = gameMGR.GetComponent<_gameMGR>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Ball hit goal " + team);
+        Team s;
+        if (team == Team.red) { s = Team.blue; } else { s = Team.red; }
+        if (col.gameObject.tag == "Ball")
+        {
+            ball b = col.gameObject.GetComponent<ball>();
+            b.Reset();
+            mgr.AddScore(s);
+             //SendMessageUpwards("AddScore", s);
+        }
+    }
+}
