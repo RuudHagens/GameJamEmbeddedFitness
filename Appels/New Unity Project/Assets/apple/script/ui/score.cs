98bb7117cf6a70b27873334911c524fa735409a7
Martijn Daniels <martijn.daniels@student.fontys.nl>
2016-05-12 13:54:47 +0200

    lll

---
+++ Appels/New Unity Project/Assets/apple/script/ui/score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class score : MonoBehaviour{
+    public GameObject playerToTrack;
+    private basket p;
+
+    void Start()
+    {
+        p = playerToTrack.GetComponent<basket>();
+    }
+
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 pp = playerToTrack.transform.position;
+        transform.position.Set(pp.x, pp.y, pp.z);
+	}
+}
